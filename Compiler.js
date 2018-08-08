ad720dbf9e2de7fc87f91151b140ae99e64689e2
libertyzhao <376968795@qq.com>
2018-08-08 20:23:50 +0800

    调试修改

--- Compiler.js
+++ Compiler.js
@@ -10,9 +10,9 @@ let lexer = new Lexer(input);
 let parser = new Parser(lexer);
 let nfaMachine = new NfaMachine(parser);
 
-let inputString = `[0-9].ab`;
+let inputString = `[0-9].ab*`;
 console.log(`输入公式：${inputString}`);
 input.setInput(preProcess.process(inputString));
 
 nfaMachine.run();
-nfaMachine.test(`01a`);
+nfaMachine.test(`01abbb`);
