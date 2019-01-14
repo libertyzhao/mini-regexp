f14acf12bbc03a18aa5a808bffb140e3ccf0487d
liberty <376968795@qq.com>
2019-01-14 23:43:55 +0800

    update

--- Compiler.js
+++ Compiler.js
@@ -1,21 +1,21 @@
-let Lexer = require('./Lexer')
-let Parser = require('./Parser')
-let Input = require('./Input')
-let PreProcess = require('./PreProcess')
-let NfaMachine = require('./NfaMachine');
+const Lexer = require('./Lexer')
+const Parser = require('./Parser')
+const Input = require('./Input')
+const PreProcess = require('./PreProcess')
+const NfaMachine = require('./NfaMachine');
 
 // 配置宏 \d  ->  [0-9]
-let preProcess = new PreProcess({d: '[0-9]'});
-let input = new Input();
-let lexer = new Lexer(input);
-let parser = new Parser(lexer);
-let nfaMachine = new NfaMachine(parser);
+const preProcess = new PreProcess({d: '[0-9]'});
+const input = new Input();
+const lexer = new Lexer(input);
+const parser = new Parser(lexer);
+const nfaMachine = new NfaMachine(parser);
 
-let regexp = `[A-Z]+\\d+(.*):(.*)+[A-Z]+\\d+`;
+const regexp = `[A-Z]+\\d+(.*):(.*)+[A-Z]+\\d+`;
 console.log(`输入公式：${regexp}`);
 input.setInput(preProcess.process(regexp));
 
-let str = `A1:B$1,C$1:D$1,E$1:F$1,G$1:H$1`
+const str = `A1:B$1,C$1:D$1,E$1:F$1,G$1:H$1`
 
 nfaMachine.run();
 let start = Date.now();
@@ -26,7 +26,7 @@ console.log('dfa正则执行耗时:' + (end - start))
 // 灾难性回溯
 const reg = new RegExp(regexp);
 start = Date.now();
-let res = reg.test(str);
+const res = reg.test(str);
 end = Date.now();
 console.log(res ? '通过' : '不通过');
 console.log('常规正则执行耗时:' + (end - start))
